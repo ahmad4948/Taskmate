be5ac77b5958916c54b3361d1db9f9918fe67098
ahmad4948 <ahmadshamekh99@gmail.com>
2025-09-09 23:12:18 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header(){
+    return(
+      <header>
+       <img src="banana.JPG"/>
+        <h1>Taskmate</h1>
+      </header>
+    );
+}
+export default Header;
