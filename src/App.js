7b1390351f517a73379e77df24501cfd03fea087
ahmad4948 <ahmadshamekh99@gmail.com>
2025-09-10 04:41:20 +0300

    add task design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import Header from './components/Header';
 import './App.css';
+import AddTask from './components/AddTask';
 
 
 function App() {
   return (
     <>
     <Header />
-      
+      <AddTask/>
     </>
   );
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function AddTask(){
+    return (
+       <div className='add'>
+        <input placeholder='add task'></input>
+        <button>Add</button>
+       </div>
+    );
+}
+export default AddTask;
